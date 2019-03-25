23bbb910519d235b4ffaafd0b3fdb7365f9ff37a
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-03-25 00:27:32 -0300

    fix: body-parser middleware

--- src/config/app.js
+++ src/config/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import routes from './routes';
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(routes);
 
